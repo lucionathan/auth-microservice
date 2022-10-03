9c3bc25ca40c9dee556e0d0f56c83ae8505a350e
Lucio Nathan <lucio.matias@ccc.ufcg.edu.br>
2022-10-03 15:33:53 -0300

    sim

--- auth/src/main/java/auth/controller/AuthController.java
+++ auth/src/main/java/auth/controller/AuthController.java
@@ -3,6 +3,7 @@ package auth.controller;
 import auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,4 +24,10 @@ public class AuthController {
     public ResponseEntity<?> validate(@RequestHeader String token) {
         return authService.validate(token);
     }
+
+    @GetMapping
+    public void test() throws Exception {
+        authService.test();
+    }
+
 }
