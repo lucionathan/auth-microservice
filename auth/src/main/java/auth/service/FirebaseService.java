53cc4ec1720fd2068c120fc4c80229c01f47fd79
viniFurukawa <vinicius.carvalho@opus-software.com.br>
2022-10-12 19:06:39 -0300

    Adding readme and dockerfile updated

--- auth/src/main/java/auth/service/FirebaseService.java
+++ auth/src/main/java/auth/service/FirebaseService.java
@@ -1,19 +1,14 @@
 package auth.service;
 
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
-import static com.google.auth.oauth2.GoogleCredentials.*;
+import static com.google.auth.oauth2.ServiceAccountCredentials.fromStream;
 import static com.google.firebase.FirebaseApp.initializeApp;
 import static com.google.firebase.cloud.FirestoreClient.*;
 
@@ -22,19 +17,16 @@ public class FirebaseService {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public static final String SERVICE_ACCOUNT_FILE = "serviceAccount.json";
+    public static final String SERVICE_ACCOUNT_FILE = "/serviceAccount.json";
 
     private Firestore app;
 
     public FirebaseService() {
         logger.debug("m=Firebase stage=init");
         try {
-            var classloader = Thread.currentThread().getContextClassLoader();
-            var file = new File(classloader.getResource(SERVICE_ACCOUNT_FILE).getFile());
-            var serviceAccount = new FileInputStream(file);
-
+            var inputStream = this.getClass().getResourceAsStream(SERVICE_ACCOUNT_FILE);
             var options = FirebaseOptions.builder()
-                    .setCredentials(fromStream(serviceAccount))
+                    .setCredentials(fromStream(inputStream))
                     .build();
 
             initializeApp(options);
